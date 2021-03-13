3809c3101da549c70a3727d6e6de8472b47c3249
Bulex <abulyg1n@yandex.ru>
2021-03-13 11:03:28 +0300

    New project fot git

---
+++ ConsoleAppGitt/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppGitt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello git!");
+            Console.WriteLine("Almost forgot, hello world!");
+            Console.WriteLine("Goodbuy, world!");
+        }
+    }
+}
